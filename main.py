421e24a30309a155009edc50a453df5adc41da46
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-20 08:53:01 -0500

    Made base

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import jsonify
+
+
+app = Flask('originstorebackend')
+
+@app.route('/')
+def index():
+    return jsonify({"message": "/ endpoint"})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 8080, True)
