769db0e41467452ee169bdd7a482d3816306d5fc
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-20 09:07:27 -0500

    Added endpoit

--- main.py
+++ main.py
@@ -2,13 +2,20 @@
 
 from flask import Flask
 from flask import jsonify
+import json
 
 
 app = Flask('originstorebackend')
 
 @app.route('/')
 def index():
-    return jsonify({"message": "/ endpoint"})
+    return "Hello!!"
+
+@app.route('/applacations', methods=['GET'])
+def applacations():
+    with open('apps/applacations.json', 'r') as f:
+        data = json.load(f)
+    return jsonify(data)
 
 
 if __name__ == '__main__':
