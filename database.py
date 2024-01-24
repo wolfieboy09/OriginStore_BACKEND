14449f3a3ef4d513c368b78a2d8a5fee322a0a2c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-01-24 17:40:22 -0500

    Working on database

--- database.py
+++ database.py
@@ -2,14 +2,44 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 import LOGGER
+from bson import ObjectId
 
 uri = os.environ['MONGODB_URI']
 client = MongoClient(uri, server_api=ServerApi('1'))
-USERS = client['UserInfo']
-user_coll = USERS['UserInfo']
 
 def doesUserExist(username: str) -> bool:
+    user_coll = client.get_database()['OSLapps']
     user_document = user_coll.find_one({"username": username})
     if user_document: return True
     else: return False   
+    
+    
+def newApp(name: str, author: str, current_ver: float, description: str, author_link_types: list, links: list, required_ver: float) -> bool:
+    app_coll = client.get_database()['OSLapps']
+    INSERT = {
+        "_id": ObjectId(),
+        "name": name,
+        "author": author,
+        "current_var": f"{current_ver}",
+        "description": description,
+        "author_link_types": f"{author_link_types}",
+        "required_ver": f"{required_ver}",
+        "links": f"{links}"
+        }
+    app_coll.insert_one(INSERT)
+    return True
+
+
+def getAllApps():
+    collection = client.get_database()['OSLapps']
+    result = collection.find({}, {'_id': 1, 'name': 1, 'author': 1, 'current_var': 1, 'description': 1, 'author_link_types': 1, 'links': 1, 'required_ver': 1})
+    apps_list = []
+    for document in result:
+        apps_list.append(document)
+    response_json = {"apps": apps_list}
+    return response_json
+
+   
+
+        
     
